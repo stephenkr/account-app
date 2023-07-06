b47482ce28b24ee58a851a222916ec84d2ff199a
Stephen K <stephenkerr0312@gmail.com>
2023-07-06 16:58:39 +0200

    refactor: change seed tags

--- packages/api/scripts/seeds/accounts.ts
+++ packages/api/scripts/seeds/accounts.ts
@@ -4,8 +4,8 @@ import mongoose from 'mongoose';
 export const accountDocuments = Array.from(new Array(25), () => ({
   _id: new mongoose.mongo.ObjectId(faker.database.mongodbObjectId()),
   name: faker.finance.accountName(),
-  category: faker.string.alpha(10),
-  tags: [faker.string.alpha(5), faker.string.alpha(5)],
+  category: faker.animal.dog(),
+  tags: [faker.animal.bird(), faker.animal.bird()],
   balance: faker.number.float({ min: 0, max: 100 }),
   availableBalance: faker.number.float({ min: 0, max: 100 }),
 }))
